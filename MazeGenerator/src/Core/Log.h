4295ec7c9b527196ee68159b668dc0bc565df446
Ruan C. Keet <26340461@sun.ac.za>
2023-05-19 14:17:58 +0200

    Reformatted file

--- MazeGenerator/src/Core/Log.h
+++ MazeGenerator/src/Core/Log.h
@@ -4,12 +4,11 @@
 
 #pragma once
 
-#ifdef MG_DEBUG
-#include <iostream>
-#include <format>
-#define LOG(...) std::cout << "[DEBUG]: " << std::format(__VA_ARGS__) << std::endl
-
-#elif MG_RELEASE
-#define LOG(...)
-
+#if defined(MG_DEBUG)
+	#include <iostream>
+	#include <format>
+	
+	#define LOG(...) std::cout << "[DEBUG]: " << std::format(__VA_ARGS__) << std::endl
+#elif defined(MG_RELEASE)
+	#define LOG(...)
 #endif
