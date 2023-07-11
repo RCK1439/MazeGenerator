f87276bb67bfa9a1b5654fca9802593cab02be3d
Ruan C. Keet <rckeet@gmail.com>
2023-07-11 21:44:32 +0200

    Reformatted

--- MazeGenerator/src/Core/Platform.h
+++ MazeGenerator/src/Core/Platform.h
@@ -6,13 +6,13 @@
 #pragma once
 
 #if defined(_WIN32)
-	#if defined(_WIN64)
-		#define MG_PLATFORM_WINDOWS
-	#else
-		#error "x86 builds not supported"
-	#endif
+#	if defined(_WIN64)
+#		define MG_PLATFORM_WINDOWS
+#	else
+#		error "x86 builds not supported"
+#	endif
 #elif defined(__linux__)
-	#define MG_PLATFORM_LINUX
+#	define MG_PLATFORM_LINUX
 #elif defined(__APPLE__)
-	#error "MacOS not supported"
+#	error "MacOS not supported"
 #endif
