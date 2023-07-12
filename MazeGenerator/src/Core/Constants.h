4b4c9152dc31a64c930cd6f62cd7b03b5eb12614
Ruan C. Keet <rckeet@gmail.com>
2023-07-12 12:03:14 +0200

    Added version specifier

--- MazeGenerator/src/Core/Constants.h
+++ MazeGenerator/src/Core/Constants.h
@@ -9,12 +9,14 @@
 
 namespace maze
 {
-	constexpr const u8 MAX_GENERATION_SPEED = 64;
-	constexpr const u8 MIN_GENERATION_SPEED =  1;
+	constexpr const char* MAZE_VERSION = "1.1";
 
-	constexpr const u8 MAX_CELL_SIZE = 64;
-	constexpr const u8 MIN_CELL_SIZE =  8;
+	constexpr u8 MAX_GENERATION_SPEED = 64;
+	constexpr u8 MIN_GENERATION_SPEED =  1;
 
-	constexpr const u32 SCREEN_WIDTH  = 1536;
-	constexpr const u32 SCREEN_HEIGHT = 768;
+	constexpr u8 MAX_CELL_SIZE = 64;
+	constexpr u8 MIN_CELL_SIZE =  8;
+
+	constexpr u32 SCREEN_WIDTH  = 1536;
+	constexpr u32 SCREEN_HEIGHT = 768;
 }
