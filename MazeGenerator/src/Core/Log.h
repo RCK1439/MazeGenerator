72e0323677ed91e2e37a081555bceac5f417c9cf
Ruan C. Keet <rckeet@gmail.com>
2023-07-12 17:18:03 +0200

    Logger only uses printf now

--- MazeGenerator/src/Core/Log.h
+++ MazeGenerator/src/Core/Log.h
@@ -6,16 +6,9 @@
 #pragma once
 
 #if defined(MG_DEBUG)
-	#if defined(_MSC_VER)
-		#include <iostream>
-		#include <format>
+	#include <stdio.h>
 
-		#define LOG(...) std::cout << "[DEBUG]: " << std::format(__VA_ARGS__) << std::endl
-	#elif defined(__GNUC__)
-		#include <stdio.h>
-
-		#define LOG(...) printf(__VA_ARGS__); printf("\n")
-	#endif
+	#define LOG(...) printf(__VA_ARGS__); printf("\n")
 #elif defined(MG_RELEASE)
 	#define LOG(...)
 #endif
